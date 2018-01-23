1bc287066518599b15708466e7c98e2502a820b0
Your Name <anhht@haposoft.com>
2018-01-23 11:01:11 +0700

    fix carousel

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 $(document).ready(function () {
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
+    var sync1 = $('#sync1');
+    var sync2 = $('#sync2');
+    sync1.owlCarousel({
         items: 8,
         loop: true,
-        margin: 10,
-        autoplay: true,
+        padding: 10,
+        autoplay: false,
         autoplayTimeout: 5000,
         autoplayHoverPause: true
     });
+    sync2.owlCarousel({
+        items: 1,
+        dots: false,
+    });
+    sync1.on('change.owl.carousel', function (event) {
+        sync2.trigger('next.owl.carousel');
+    })
+    sync2.on('change.owl.carousel', function (event) {
+        sync1.trigger('next.owl.carousel');
+    })
     $('.play').on('click', function () {
         owl.trigger('play.owl.autoplay', [1000])
     })
